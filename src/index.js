4927953d830aa71b8d8aacf6ebe9861f35b31789
Anton <anton@adc.sh>
2019-05-13 12:13:56 +0300

    src and extern

--- src/index.js
+++ src/index.js
@@ -1,31 +1,23 @@
-import { debuglog } from 'util'
+export default preact
 
-const LOG = debuglog('@externs/preact')
+const {
+  h,
+  createElement,
+	cloneElement,
+	createRef,
+	Component,
+	render,
+	rerender,
+	options,
+} = preact
 
-/**
- * The Externs For Preact.
- * @param {_@externs/preact.Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
- */
-export default async function preact(config = {}) {
-  const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('@externs/preact called with %s', text)
-  return text
+export {
+  h,
+  createElement,
+	cloneElement,
+	createRef,
+	Component,
+	render,
+	rerender,
+	options,
 }
-
-/* documentary types/index.xml */
-/**
- * @suppress {nonStandardJsDocs}
- * @typedef {_@externs/preact.Config} Config Options for the program.
- */
-/**
- * @suppress {nonStandardJsDocs}
- * @typedef {Object} _@externs/preact.Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
- */
--- types/externs.js
+++ types/externs.js
@@ -1,5 +1,148 @@
-/* typal types/index.xml externs */
 /**
- * @typedef {{ shouldRun: (boolean|undefined), text: string }}
+ * @const
+ * @record
  */
-var Config
+var preact = {}
+
+/**
+ * @param {string|!Function} nodeName An element name. Ex: `div`, `a`, `span`, etc.
+ * @param {Object} [attributes] Any attributes/props to set on the created element.
+ * @param {...!(preact.VNode|Array<!preact.VNode>)} args Additional arguments are taken to be children to append. Can be infinitely nested Arrays.
+ * @return {!preact.VNode}
+ */
+preact.h = function(nodeName, attributes, ...args) {}
+/**
+ * @param {string|!Function} nodeName An element name. Ex: `div`, `a`, `span`, etc.
+ * @param {Object} [attributes] Any attributes/props to set on the created element.
+ * @param {...!(preact.VNode|Array<!preact.VNode>)} args Additional arguments are taken to be children to append. Can be infinitely nested Arrays.
+ * @return {!preact.VNode}
+ */
+preact.createElement = function(nodeName, attributes, ...args) {}
+/**
+ * Clones the given VNode, optionally adding attributes/props and replacing its
+ * children.
+ * @param {!preact.VNode} vnode The virtual DOM element to clone.
+ * @param {!Object} [props] Attributes/props to add when cloning.
+ * @param {...!(preact.VNode|Array<!preact.VNode>)} args Any additional arguments will be used as replacement children.
+ * @return {!preact.VNode}
+ */
+preact.cloneElement = function(vnode, props, args) {}
+/**
+ * Just returns an object...?
+ * @return {!Object}
+ */
+preact.createRef = function() {}
+/**
+ * Render JSX into a `parent` Element.
+ * @param {!preact.VNode} vnode A (JSX) VNode to render
+ * @param {!Element} parent DOM element to render into
+ * @param {!Element} [merge] Attempt to re-use an existing DOM tree rooted at `merge`
+ */
+preact.render = function(vnode, parent, merge) {}
+/**
+ * Rerenders all rendered elements.
+ */
+preact.rerender = function() {}
+
+/** @record */
+preact.options
+/** @type {boolean} */
+preact.options.syncComponentUpdates
+/** @type {function(function(): void): void} */
+preact.options.debounceRendering
+/** @type {function(!preact.VNode): void} */
+preact.options.vnode
+/** @type {function(!Event): !Event} */
+preact.options.event
+/** @type {function(!preact.Component): void} */
+preact.options.afterMount
+/** @type {function(!preact.Component): void} */
+preact.options.afterUpdate
+/** @type {function(!preact.Component): void} */
+preact.options.beforeUnmount
+
+/**
+ * Virtual DOM Node.
+ * @constructor
+ */
+preact.VNode = function(){}
+/**
+ * The string of the DOM node to create or Component constructor to render.
+ * @type {string|Function}
+ */
+preact.VNode.prototype.nodeName
+/**
+ * The children of node.
+ * @type {Array<!VNode|string>}
+ */
+preact.VNode.prototype.children
+/**
+ * The key used to identify this VNode in a list.
+ * @type {string|number|undefined}
+ */
+preact.VNode.prototype.children
+/**
+ * The properties of this VNode.
+ * @type {Object}
+ */
+preact.VNode.prototype.attributes
+
+/**
+ * @param {!Object} [props] The initial component props
+ * @param {!Object} [context] The initial context from parent components' getChildContext
+ */
+preact.Component = function(props, context) {}
+/** @type {!Object} */
+preact.Component.prototype.context
+/** @type {!Object} */
+preact.Component.prototype.props
+/** @type {!Object} */
+preact.Component.prototype.state
+/**
+ * @param {!Object} state
+ * @param {function(): void} callback
+ */
+preact.Component.prototype.setState = function(state, callback) {}
+/**
+ * @param {!Object} state
+ * @param {function(): void} callback
+ */
+preact.Component.prototype.forceUpdate = function(callback) {}
+/**
+ * @param {!Object} props
+ * @param {!Object} state
+ * @param {!Object} context
+ * @return {!preact.VNode}
+ */
+preact.Component.prototype.render = function(props, state, context) {}
+preact.Component.prototype.componentWillMount = function() {}
+preact.Component.prototype.componentDidMount = function() {}
+preact.Component.prototype.componentWillUnmount = function() {}
+/**
+ * @returns {!Object}
+ */
+preact.Component.prototype.getChildContext = function() {}
+/**
+ * @param {!Object} [nextProps]
+ * @param {!Object} [nextContext]
+ */
+preact.Component.prototype.componentWillReceiveProps = function(nextProps, nextContext) {}
+/**
+ * @param {!Object} [nextProps]
+ * @param {!Object} [nextState]
+ * @param {!Object} [nextContext]
+ * @return {boolean}
+ */
+preact.Component.prototype.shouldComponentUpdate = function(nextProps, nextState, nextContext) {}
+/**
+ * @param {!Object} [nextProps]
+ * @param {!Object} [nextState]
+ * @param {!Object} [nextContext]
+ */
+preact.Component.prototype.componentWillUpdate = function(nextProps, nextState, nextContext) {}
+/**
+ * @param {!Object} [prevProps]
+ * @param {!Object} [prevState]
+ * @param {!Object} [prevContext]
+ */
+preact.Component.prototype.componentDidUpdate = function(prevProps, prevState, prevContext) {}
