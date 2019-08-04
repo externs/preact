3b324225450621701c9201b17fb9694e74e61f31
Anton <anton@adc.sh>
2019-08-04 18:05:23 +0300

    start new externs

---
+++ types/e.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview
+ * @externs
+ */
+
+/* typal types/index.xml externs */
+/** @const */
+var _preact = {}
+/**
+ * The pragma (rendering) function.
+ * @param {string|!Function} nodeName An element name. Ex: `div`, `a`, `span`, etc.
+ * @param {Object=} [attributes] Any attributes/props to set on the created element.
+ * @param {...*} args Additional arguments are taken to be children to append. Can be infinitely nested Arrays.
+ */
+_preact.h = function(nodeName, attributes, ...args) {}
