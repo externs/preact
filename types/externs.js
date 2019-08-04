f9090d7900931542bd4b04147fd925946168afb3
Anton <anton@adc.sh>
2019-08-04 19:19:05 +0300

    add types

--- types/externs.js
+++ types/externs.js
@@ -1,29 +1,35 @@
+/**
+ * @fileoverview
+ * @externs
+ */
+
+/* typal types/methods.xml externs */
 /** @const */
 var preact = {}
-
 /**
+ * The pragma (rendering) function.
  * @param {string|!Function} nodeName An element name. Ex: `div`, `a`, `span`, etc.
- * @param {Object} [attributes] Any attributes/props to set on the created element.
- * @param {...*} args Additional arguments are taken to be children to append. Can be infinitely nested Arrays.
+ * @param {Object=} [attributes] Any attributes/props to set on the created element.
+ * @param {...!(preact.VNode|Array<!preact.VNode>)} args Additional arguments are taken to be children to append. Can be infinitely nested Arrays.
  * @return {!preact.VNode}
  */
 preact.h = function(nodeName, attributes, ...args) {}
 /**
+ * The pragma (rendering) function. Alias of `h`.
  * @param {string|!Function} nodeName An element name. Ex: `div`, `a`, `span`, etc.
- * @param {Object} [attributes] Any attributes/props to set on the created element.
+ * @param {Object=} [attributes] Any attributes/props to set on the created element.
  * @param {...!(preact.VNode|Array<!preact.VNode>)} args Additional arguments are taken to be children to append. Can be infinitely nested Arrays.
  * @return {!preact.VNode}
  */
 preact.createElement = function(nodeName, attributes, ...args) {}
 /**
- * Clones the given VNode, optionally adding attributes/props and replacing its
- * children.
+ * Clones the given VNode, optionally adding attributes/props and replacing its children.
  * @param {!preact.VNode} vnode The virtual DOM element to clone.
- * @param {!Object} [props] Attributes/props to add when cloning.
+ * @param {Object=} [props] Attributes/props to add when cloning.
  * @param {...!(preact.VNode|Array<!preact.VNode>)} args Any additional arguments will be used as replacement children.
  * @return {!preact.VNode}
  */
-preact.cloneElement = function(vnode, props, args) {}
+preact.cloneElement = function(vnode, props, ...args) {}
 /**
  * Just returns an object...?
  * @return {!Object}
@@ -33,7 +39,7 @@ preact.createRef = function() {}
  * Render JSX into a `parent` Element.
  * @param {!preact.VNode} vnode A (JSX) VNode to render.
  * @param {Element} parent DOM element to render into.
- * @param {Element} [merge] Attempt to re-use an existing DOM tree rooted at `merge`.
+ * @param {Element=} [merge] Attempt to re-use an existing DOM tree rooted at `merge`.
  */
 preact.render = function(vnode, parent, merge) {}
 /**
@@ -41,31 +47,63 @@ preact.render = function(vnode, parent, merge) {}
  */
 preact.rerender = function() {}
 
-/** @record */
+// because each option is optional, they are defined as types and not functions
+// to allow "|undefined".
+// todo typal: write constructor still ( = function(arg,arg2) {})
+/* typal types/options.xml externs */
+/**
+ * Options for Preact.
+ * @record
+ */
 preact.options
-/** @type {boolean} */
-preact.options.syncComponentUpdates
-/** @type {function(function(): void): void} */
-preact.options.debounceRendering
-/** @type {function(!preact.VNode): void} */
-preact.options.vnode
-/** @type {function(!Event): !Event} */
-preact.options.event
-/** @type {function(!preact.Component): void} */
-preact.options.afterMount
-/** @type {function(!preact.Component): void} */
-preact.options.afterUpdate
-/** @type {function(!preact.Component): void} */
-preact.options.beforeUnmount
+/**
+ * @type {boolean|undefined}
+ */
+preact.options.prototype.syncComponentUpdates
+/**
+ * @type {(function(function(): void): void)|undefined}
+ */
+preact.options.prototype.debounceRendering = function() {}
+/**
+ * @type {(function(!preact.VNode): void)|undefined}
+ */
+preact.options.prototype.vnode = function() {}
+/**
+ * @type {(function(!Event): !Event)|undefined}
+ */
+preact.options.prototype.event = function() {}
+/**
+ * @type {(function(!preact.Component): void)|undefined}
+ */
+preact.options.prototype.afterMount = function() {}
+/**
+ * @type {(function(!preact.Component): void)|undefined}
+ */
+preact.options.prototype.afterUpdate = function() {}
+/**
+ * @type {(function(!preact.Component): void)|undefined}
+ */
+preact.options.prototype.beforeUnmount = function() {}
 
+/* typal types/vnode.xml externs */
+/**
+ * A component that extends preact.Component to set default properties. https://git.io/fjHoZ
+ * @interface
+ */
+preact._Component = function() {}
+/**
+ * The properties that will be assigned on the component by _Preact_ when constructing it.
+ * @type {!Object}
+ */
+preact._Component.prototype.defaultProps
 /**
  * Virtual DOM Node.
- * @constructor
+ * @interface
  */
-preact.VNode = function(){}
+preact.VNode = function() {}
 /**
  * The string of the DOM node to create or Component constructor to render.
- * @type {string|Function}
+ * @type {string|preact._Component}
  */
 preact.VNode.prototype.nodeName
 /**
@@ -75,7 +113,7 @@ preact.VNode.prototype.nodeName
 preact.VNode.prototype.children
 /**
  * The key used to identify this VNode in a list.
- * @type {string|number|undefined}
+ * @type {(string|number)|undefined}
  */
 preact.VNode.prototype.key
 /**
@@ -83,6 +121,7 @@ preact.VNode.prototype.key
  * @type {Object}
  */
 preact.VNode.prototype.attributes
+
 
 /**
  * @constructor
